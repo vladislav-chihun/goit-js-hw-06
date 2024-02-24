956f16196f870f8acb049d90838bb162b1544ab4
vladislav-chihun <vladikkosmo@gmail.com>
2024-02-24 17:35:07 +0100

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -28,4 +28,3 @@ console.log(customer.getDiscount()); // 0.15
 customer.addOrder(5000, "Steak");
 console.log(customer.getBalance()); // 19750
 console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
-/
